b62a4215e7cfeb4fc2fb638abb4d9966d1c7e5ba
Prashant Srivastav <srivastavp891@gmail.com>
2024-06-22 21:31:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+// Endpoint to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
